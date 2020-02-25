f15c1cf1ce206c0e7b0a768515e9c3f09cc88190
Charles <ghanime.charles@gmail.com>
2020-02-25 20:19:14 +0100

    norme visu.corewar.c

--- VM/src/visu/visu_corewar.c
+++ VM/src/visu/visu_corewar.c
@@ -6,14 +6,14 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 16:52:07 by cghanime          #+#    #+#             */
-/*   Updated: 2020/02/25 19:21:30 by cghanime         ###   ########.fr       */
+/*   Updated: 2020/02/25 19:28:41 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "op.h"
 #include "libft.h"
 
-void	print_dashboard(t_visu *visu)
+void			print_dashboard(t_visu *visu)
 {
 	int32_t i;
 
@@ -24,33 +24,35 @@ void	print_dashboard(t_visu *visu)
 	mvwprintw(visu->dash, 29, 7, "Nb Cycles : %4d", visu->cor->nb_cycles);
 	while (++i < (int)visu->cor->nb_champs)
 		mvwprintw(visu->dash, 30 + i, 7, "Champ %d last cycle to live : %d",
-			i + 1, visu->cor->champ[i].last_cycle_live);
+	i + 1, visu->cor->champ[i].last_cycle_live);
 	nanosleep(&visu->time1, &visu->time2);
-	mvwprintw(visu->dash, 34, 7, "Cycle to die : %4d", visu->cor->cycles_to_die);
+	mvwprintw(visu->dash, 34, 7, "Cycle to die : %4d",
+	visu->cor->cycles_to_die);
 	mvwprintw(visu->dash, 35, 7, "Cycle to die ref : %4d",
-		visu->cor->cycles_to_die_ref);
+	visu->cor->cycles_to_die_ref);
 }
 
- void	print_winner(t_visu *visu, int ind_winner)
- {
-	 char c;
+void			print_winner(t_visu *visu, int ind_winner)
+{
+	char c;
 
-	 wattron(visu->dash, COLOR_PAIR(6));
-	 mvwprintw(visu->dash, 50, 7, "And the winner is ... Champion %s", visu->cor->champ[ind_winner].header.prog_name);
-	 wrefresh(visu->dash);
-	 while (read(0, &c, 1) != -1 &&  c != ' ')
-	 	;
- }
+	wattron(visu->dash, COLOR_PAIR(6));
+	mvwprintw(visu->dash, 50, 7, "And the winner is ... Champion %s",
+			visu->cor->champ[ind_winner].header.prog_name);
+	wrefresh(visu->dash);
+	while (read(0, &c, 1) != -1 && c != ' ')
+		;
+}
 
-static void	kill_window(void)
+static void		kill_window(void)
 {
 	endwin();
 }
 
-int		ft_visu(t_cor *cor, t_visu *visu)
+int				ft_visu(t_cor *cor, t_visu *visu)
 {
-	int8_t		fd_1;
-	int8_t		fd_2;
+	int8_t fd_1;
+	int8_t fd_2;
 
 	if (!(fd_1 = call_open("./src/visu/visu_utils/corewar")))
 		return (1);
